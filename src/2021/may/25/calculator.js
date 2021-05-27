a2f474390abe661dda95829f19fd98c1fea94ba8
Xaver Davey <xaver.rd@gmail.com>
2021-05-27 16:29:23 +0200

    Moved solutions to new forked repo (this repo)

--- src/2021/may/25/calculator.js
+++ src/2021/may/25/calculator.js
@@ -3,8 +3,114 @@
 // The string will contain the operators: +, -, /, *, (, and ). If you have a string like this: #/#*# or #+#(#)/#, then evaluate from left to right. So divide then multiply, and for the second one multiply, divide, then add. The evaluations will be such that there will not be any decimal operations, so you do not need to account for rounding and whatnot.
 
 function calculator(str) {
-  // code goes here
-  return str;
+
+  var i; // variable for iteration
+  var operands = []; // contains operands (as stings) in order
+  var operators = []; // contains operators (as strings) in order
+
+  str = "@" + str;
+
+  for (i = 0; i < str.length; ++i) {
+
+    // handle operators
+    if (str.charAt(i) === '(') { // bracket multiplication sytnax
+      operators.push('*');
+    } else {
+      operators.push(str.charAt(i));
+      ++i;
+    }
+
+    // handle operands
+    if (str.charAt(i) === '(') { // if bracket operand
+      var substring = getBracketContents(str, i + 1);
+      i += substring.length + 1;
+      substring = calculator(substring);
+    } else { // if positive operand
+      var substring = getNumber(str, i);
+      i += substring.length - 1;
+    }
+    operands.push(substring);
+
+  }
+
+  // first go through multiplication and divison
+  for (i = 0; i < operators.length; ++i) {
+    if (operators[i] === '*' || operators[i] === '/') {
+      operands[i - 1] = calculate(operators[i], operands[i - 1], operands[i]);
+      operators.splice(i, 1);
+      operands.splice(i, 1);
+      --i;
+    }
+  }
+
+  // then go through addition and subtraction
+  for (i = 0; i < operators.length; ++i) {
+    if (operators[i] === '+' || operators[i] === '-') {
+      operands[i - 1] = calculate(operators[i], operands[i - 1], operands[i]);
+      operators.splice(i, 1);
+      operands.splice(i, 1);
+      --i;
+    }
+  }
+
+  return parseInt(operands[0]);
+
 }
+
+
+
+
+function getNumber(str, i) {
+  var number = "";
+  if (str.charAt(i) === '-') {
+    number += '-';
+    ++i;
+  }
+  while (str.charAt(i) >= '0' && str.charAt(i) <= '9') {
+    number += str.charAt(i);
+    ++i;
+  }
+  return number;
+}
+
+
+
+
+function getBracketContents(str, i) {
+  var result = "";
+  var numEmbeddedBrackets = 0;
+  while (str.charAt(i) !== ')' || numEmbeddedBrackets !== 0) {
+    if (str.charAt(i) === '(') {
+      ++numEmbeddedBrackets;
+    } else if (str.charAt(i) === ')') {
+      --numEmbeddedBrackets;
+    }
+    result += str.charAt(i);
+    ++i;
+  }
+  return result;
+}
+
+
+
+
+function calculate(operation, operandA, operandB) {
+  var result;
+  var a = parseInt(operandA);
+  var b = parseInt(operandB);
+  if (operation === '*') {
+    result = a * b;
+  } else if (operation === '/') {
+    result = a / b;
+  } else if (operation === '+') {
+    result = a + b;
+  } else if (operation === '-') {
+    result = a - b;
+  }
+  return result.toString();
+}
+
+
+
 
 module.exports = calculator;
--- src/2021/may/26/sudokuQuadrantChecker.js
+++ src/2021/may/26/sudokuQuadrantChecker.js
@@ -2,9 +2,115 @@
 // For example, if strArr is: ["(1,2,3,4,5,6,7,8,1)","(x,x,x,x,x,x,x,x,x)","(x,x,x,x,x,x,x,x,x)","(1,x,x,x,x,x,x,x,x)","(x,x,x,x,x,x,x,x,x)","(x,x,x,x,x,x,x,x,x)","(x,x,x,x,x,x,x,x,x)","(x,x,x,x,x,x,x,x,x)","(x,x,x,x,x,x,x,x,x)"] then your program should return 1,3,4 since the errors are in quadrants 1, 3 and 4 because of the repeating integer 1.
 // Another example, if strArr is: ["(1,2,3,4,5,6,7,8,9)","(x,x,x,x,x,x,x,x,x)","(6,x,5,x,3,x,x,4,x)","(2,x,1,1,x,x,x,x,x)","(x,x,x,x,x,x,x,x,x)","(x,x,x,x,x,x,x,x,x)","(x,x,x,x,x,x,x,x,x)","(x,x,x,x,x,x,x,x,x)","(x,x,x,x,x,x,x,x,9)"] then your program should return 3,4,5,9.
 
+
+
+
 function sudokuQuadrantChecker(strArray) {
-  // Code goes here
-  return strArray;
+
+  var array = getEmbeddedArray(strArray);
+
+  var currentCells = []; // array of cells items being checked
+  var errorQuadrants = []; // quadrants with errors in them
+
+  var i; // variable for iterating in for loops
+  var j; // variable for iterating in for loops
+  var m; // variable for iterating in for loops
+  var n; // variable for iterating in for loops
+
+  // check rows
+  for (i = 0; i < array.length; ++i) {
+    currentCells = [];
+    for (j = 0; j < array[i].length; ++j) {
+      var repeat = getRepeat(currentCells, array[i][j]);
+      if (repeat !== null) {
+        errorQuadrants.push(getQuadrant(i, repeat));
+        errorQuadrants.push(getQuadrant(i, j));
+      } else if (array[i][j] !== 'x') {
+        currentCells.push(array[i][j]);
+      }
+    }
+  }
+
+  // check columns
+  for (j = 0; j < array[0].length; ++j) {
+    currentCells = [];
+    for (i = 0; i < array.length; ++i) {
+      var repeat = getRepeat(currentCells, array[i][j]);
+      if (repeat !== null) {
+        errorQuadrants.push(getQuadrant(repeat, j));
+        errorQuadrants.push(getQuadrant(i, j));
+      } else if (array[i][j] !== 'x') {
+        currentCells.push(array[i][j]);
+      }
+    }
+  }
+
+  // check subgrids
+  for (m = 0; m < 9; m += 3) {
+    for (n = 0; n < 9; n += 3) {
+      currentCells = [];
+      for (i = m; i < m + 3; ++i) {
+        for (j = n; j < n + 3; ++j) {
+          var repeat = getRepeat(currentCells, array[i][j]);
+          if (repeat !== null) {
+            errorQuadrants.push(getQuadrant(i, j));
+          }
+          currentCells.push(array[i][j]);
+        }
+      }
+    }
+  }
+
+  // clean up and return
+  var representation = '';
+  errorQuadrants = Array.from(new Set(errorQuadrants)).sort();
+  for (i = 0; i < errorQuadrants.length; ++i) {
+    representation += ',';
+    representation += errorQuadrants[i];
+  }
+  if (representation === '') {
+    return 'legal'
+  } else {
+    return representation.substring(1, representation.length);
+  }
+
 }
+
+
+
+
+
+function getEmbeddedArray(strArray) {
+  var array = [];
+  var i;
+  for (i = 0; i < strArray.length; ++i) {
+    var substr = strArray[i];
+    array.push(substr.substring(1, substr.length - 1).split(','));
+  }
+  return array;
+}
+
+
+
+
+function getRepeat(subArray, cell) {
+  var i;
+  for (i = 0; i < subArray.length; ++i) {
+    if (subArray[i] === cell && cell !== 'x') {
+      return i;
+    }
+  }
+  return null;
+}
+
+
+
+
+function getQuadrant(i, j) {
+    return 3 * Math.floor(i / 3) + Math.floor(j / 3) + 1;
+}
+
+
+
 
 module.exports = sudokuQuadrantChecker;
