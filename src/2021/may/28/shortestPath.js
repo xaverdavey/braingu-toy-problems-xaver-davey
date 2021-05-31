7fdb7d6bda7952c9b7b0c167db48a13c79ee82c7
Xaver Davey <xaver.rd@gmail.com>
2021-05-31 16:55:05 +0200

    rudimentary solution to 28/05/2020 toy problem

--- src/2021/may/28/shortestPath.js
+++ src/2021/may/28/shortestPath.js
@@ -4,8 +4,75 @@
 // There will only ever be one shortest path for the array. If no path between the first and last node exists, return -1. The array will at minimum have two nodes. Also, the connection A-B for example, means that A can get to B and B can get to A.
 
 function shortestPath(strArr) {
-  // code goes here
-  return strArr;
+  const keyNum = 1 + parseInt(strArr[0]);
+  var map = strArr.slice(1, keyNum);
+  var paths = strArr.slice(keyNum, strArr.length);
+  const pathLength = paths.length;
+  for (let i = 0; i < pathLength; ++i) {
+    paths.push(getTo(paths[i]) + "-" + getFrom(paths[i]));
+  }
+  var routes = recurse(map, paths, map[0]);
+  //console.log(routes);
+  if (routes.length > 0) {
+    return routes[getMinPath(routes)];
+  }
+  return -1;
 }
+
+
+function recurse(map, paths, current) {
+  var routes = [];
+
+  // base case
+  if (current === map[map.length - 1]) {
+    return [current];
+  } else if (paths.length === 0) {
+    return -1;
+  }
+
+  for (let i = 0; i < paths.length; ++i) {
+    if (getFrom(paths[i]) === current) {
+      let newPaths = [...paths];
+      newPaths.splice(i, 1);
+      const furtherRoutes = recurse(map, newPaths, getTo(paths[i]));
+      //console.log(furtherRoutes);
+      for (let j = 0; j < furtherRoutes.length; ++j) {
+        if (furtherRoutes[j] !== -1) {
+          //console.log(furtherRoutes[j]);
+          routes.push(getFrom(paths[i]) + "-" + furtherRoutes[j]);
+        }
+      }
+    }
+  }
+
+  if (routes.length > 0) {
+    return routes;
+  }
+  return -1;
+}
+
+function getFrom(path) {
+  return path.split("-")[0];
+}
+
+function getTo(path) {
+  return path.split("-")[1];
+}
+
+function getMinPath(routes) {
+  let i;
+  let minLength = Number.POSITIVE_INFINITY;
+  let minIndex = -1;
+  for (i = 0; i < routes.length; ++i) {
+    const routeLength = routes[i].split("-").length;
+    if (routeLength < minLength) {
+      minLength = routeLength;
+      minIndex = i;
+    }
+  }
+  return minIndex;
+}
+
+//console.log(shortestPath(["3", "A", "B", "C", "B-C", "A-B"]));
 
 module.exports = shortestPath;
