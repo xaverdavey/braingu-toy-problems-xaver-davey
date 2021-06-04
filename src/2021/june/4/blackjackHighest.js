608030ab83d6f3161d70d782157f759677cc9d3e
Xaver Davey <xaver.rd@gmail.com>
2021-06-04 16:56:22 +0200

    rudimentary solution to the 04/06/2021 toy problem

--- src/2021/june/4/blackjackHighest.js
+++ src/2021/june/4/blackjackHighest.js
@@ -3,8 +3,56 @@
 // Another example would be if strArr was ["four","ten","king"], the output here should be above king. If you have a tie between a ten and a face card in your hand, return the face card as the "highest card". If you have multiple face cards, the order of importance is jack, queen, king.
 
 function blackjackHighest(strArr) {
-  // code goes here
-  return strArr;
+  const values = {
+    two : 2,
+    three : 3,
+    four : 4,
+    five : 5,
+    six : 6,
+    seven : 7,
+    eight : 8,
+    nine : 9,
+    ten : 10,
+    jack : 10,
+    queen : 10,
+    king : 10,
+    ace : 11,
+  };
+  let sum = 0;
+  let maxCard = "two";
+  let answer = "";
+  for (let i = 0; i < strArr.length; ++i) {
+    if (strArr[i] == "ace") {
+      if (!onlyAceLeft(strArr, i)) {
+        strArr.push(strArr[i]);
+        continue;
+      } else if (sum + 11 > 21) {
+        sum += 1;
+        continue;
+      }
+    }
+    if (values[strArr[i]] > values[maxCard]) {
+      maxCard = strArr[i];
+    }
+    sum += values[strArr[i]];
+  }
+  if (sum < 21) {
+    answer += "below";
+  } else if (sum > 21) {
+    answer += "above";
+  } else {
+    answer += "blackjack";
+  }
+  return answer + " " + maxCard;
+}
+
+function onlyAceLeft(strArray, i) {
+  for (let j = i + 1; j < i + 5 && j < strArray.length; ++j) {
+    if (strArray[j] != "ace") {
+      return false;
+    }
+  }
+  return true;
 }
 
 module.exports = blackjackHighest;
